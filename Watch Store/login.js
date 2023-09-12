fb49d514935a7f78babecfe96b37f5bd0ae9f270
Ashish <ashishkumarakf@gmail.com>
2023-09-12 14:24:32 +0530

    files upload on git hub

---
+++ Watch Store/login.js
@@ -0,0 +1,3 @@
+function submit(){
+    open(youtube.com)
+}
---
+++ Watch Store/shiv.c
@@ -0,0 +1,109 @@
+#include <stdio.h>
+int main(){
+   //  // int a;
+   //  // printf("enter the value of a:");
+   //  // scanf("%d",&a);
+    
+   //  // int b;
+   //  // printf("enter the value of b:");
+   //  // scanf("%d",&b);
+   //  //  int c;
+   //  // printf("enter the value of c:");
+   //  // scanf("%d",&c);
+   //  //  int d;
+   //  // printf("enter the value of d:");
+   //  // scanf("%d",&d);
+   //  // int e;
+   //  // printf("enter the value of e:");
+   //  // scanf("%d",&e);
+   //  // int f;
+   //  // printf("enter the value of f:");
+   //  // scanf("%d",&f);
+
+   //  // if(a>b && a>c && a>d && a>e a>f &&){
+   //  //   printf("a is greater ");
+   //  //   }
+   //  // else if(b>c && b>a && b>d && b>e && b>f){
+   //  //   printf(" b is greater");
+   //  // }
+   //  // else if(c>a && c>b && c>d && c>e && c>f){
+   //  //   printf(" c is greater");
+   //  // }
+   //  // else if( d>a && d>b && d>c && d>e && d>f) {
+   //  //   printf(" d is greater");
+   //  // }
+   //  // else if( e>a && e>b && e>c && e>d && e>f ) {
+   //  //   printf(" e is greater");
+   //  // }
+   //  //  else{
+   //  //   printf(" f is greater");
+   //  //  }
+
+   //   int date;
+   //  printf("enter the value of date:");
+   //  scanf("%d",&date);
+   //   int month;
+   //  printf("enter the value of month:");
+   //  scanf("%d",&month);
+   //   int year;
+   //  printf("enter the value of year:");
+   //  scanf("%d",&year);
+   //   if( date==31 && month==12){
+
+   //      printf("1/1/%d",year+1);
+   //   }
+   //   else if(date==31 && month!=12){
+   //      printf("1/%d/%d",month+1,year);
+
+   //   }else{
+   //      printf("%d/%d/%d",date+1,month,year);
+   //   }
+    
+     
+// ---------------loop-----------------------
+      
+// while (i>0){
+//          printf("%d\n",2*i-1);
+//          i--;
+//       }
+      // for(int i=0; i<100; i++){
+      //    printf("%d\n",i);
+      // }
+      // for(int i=1000; i>0; i--){
+      //    printf("%d\n",i);
+      // }
+      // for( int i=0; i<30; i++){
+      //    printf("%d\n",35*i);
+      // }
+      
+   //  for (int i = 1; i <11; i++)
+   //  {
+   //     for (int j = 1; j<11; j++
+   //     {
+   //       printf("%d\n",i*j);
+   //     }
+       
+   //  }
+// --------------------Array--------------------
+       int a[5];
+
+      // printf("%d",a[4]);
+
+      int i=0;
+      while(i<5)
+      {
+         printf("enter the value :");
+         scanf("%d",&a[i]);
+         i++;
+      }
+      
+
+   
+    
+
+      
+    
+
+     
+    return 0;
+}
